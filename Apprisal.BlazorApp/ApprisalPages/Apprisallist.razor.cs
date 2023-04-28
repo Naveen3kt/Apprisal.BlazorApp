b6d9cc82aa467839dbb10110f22dbe2a9e65f806
Naveen3kt <130737492+Naveen3kt@users.noreply.github.com>
2023-04-28 10:07:19 +0530

    ListPageChsnges

--- Apprisal.BlazorApp/ApprisalPages/Apprisallist.razor.cs
+++ Apprisal.BlazorApp/ApprisalPages/Apprisallist.razor.cs
@@ -6,18 +6,18 @@ namespace Apprisal.BlazorApp.ApprisalPages
 {
     public partial class Apprisallist : ComponentBase
     {
-        //private IApprisalService? service;
-        //private List<ApprisalModel>? apprisalModels;
+        private IApprisalService? service;
+        private List<ApprisalModel>? apprisalModels;
 
-        //public Apprisallist(IApprisalService Service)
-        //{
-        //    service = Service;
-        //}
-        //protected override async Task OnInitializedAsync()
-        //{
-        //    apprisalModels = await service.GetAll();
-        //    _ = base.OnInitializedAsync();
-        //}
+        public Apprisallist(IApprisalService Service)
+        {
+            service = Service;
+        }
+        protected override async Task OnInitializedAsync()
+        {
+            apprisalModels = await service.GetAll();
+            _ = base.OnInitializedAsync();
+        }
 
 
 
